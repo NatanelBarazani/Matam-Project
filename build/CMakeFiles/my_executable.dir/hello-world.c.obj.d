ec67608ce353c6d802bd11fae4febf4573960fa6
Natanel-Algoin <Natanel-Algoin@DESKTOP-F7D883C>
2021-05-04 11:08:05 +0300

    inital commit

---
+++ build/CMakeFiles/my_executable.dir/hello-world.c.obj.d
@@ -0,0 +1,14 @@
+CMakeFiles/my_executable.dir/hello-world.c.obj: \
+ C:\Users\Natanel-Algoin\Desktop\matam-project\hello-world.c \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/stdio.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/corecrt.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/_mingw.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/vadefs.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/TDM-GCC/x86_64-w64-mingw32/include/sec_api/stdio_s.h
---
+++ hello-world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+
+
+
+int main()
+{
+
+
+    printf("HI");
+    return 0;
+}
